4fe5a81aefab89c9a4d15d23b5bf1edf940d10f4
Kamran <kamran@email.com>
2025-01-24 19:10:44 +0500

    "Loop with Range"

---
+++ 14-LoopWithRange.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print("i")
+
+for i in range(2,21,2):
+    print(i)
+
+for i in range(21,-1,-1):
+    print(i)
+
+numb=int(input("Enter the  number  "))
+for a in range(1,11,1):
+    print(numb,"x",a,"=",numb*a)
